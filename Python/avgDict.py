60268ea16d91f03d946654d628d5409a7c6aaa85
Aditya Mewada <eng.adityamewada@gmail.com>
2025-05-21 00:23:14 +0530

    avgdict

---
+++ Python/avgDict.py
@@ -0,0 +1,13 @@
+# 
+
+
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    res = float(sum(student_marks[query_name])/len(student_marks[query_name]))
+    print(f"{res:.2f}")
